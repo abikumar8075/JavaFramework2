918ce73916612317e4021a9a64b807b8ef0cb10a
abikumar8075 <yadavabi8075@gmail.com>
2022-05-27 12:24:33 +0530

    add  new test runner class

---
+++ src/test/java/testRunner/Runner2.java
@@ -0,0 +1,14 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features=" C:\\Users\\VIDHYAK_GFMK\\eclipse-workspace\\CucumberWithPom\\src\\main\\java\\feature\\freeCrm.feature",
+glue={"com/qa/stepdefination"},monochrome=false,strict=true,dryRun=false)
+
+public class Runner2 {
+
+}
